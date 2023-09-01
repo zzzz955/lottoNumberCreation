a941b94fa062b99f41b261fa9c4b6e50576390d9
zzzz955 <zzzzz955@gmail.com>
2023-09-01 20:07:26 +0900

    QLabel ui 모듈 분리

--- dialog.py
+++ dialog.py
@@ -1,41 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, \
     QGridLayout, QLabel
-from PyQt5.QtGui import QPainter, QBrush, QColor, QFont
 from PyQt5.QtCore import Qt
 import random_func as ran
-
-
-class CircleLabel(QLabel):
-    def __init__(self, text):
-        super().__init__()
-
-        self.text = text
-        self.setStyleSheet('font-weight: bold;')
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        circle_rect = self.rect().translated(0, 0)
-
-        if 1 <= int(self.text) < 10:
-            brush = QBrush(QColor('#F2B720'))
-        elif 11 <= int(self.text) < 20:
-            brush = QBrush(QColor('#4072AC'))
-        elif 21 <= int(self.text) < 30:
-            brush = QBrush(QColor('#DE4C0E'))
-        elif 31 <= int(self.text) < 40:
-            brush = QBrush(QColor('#9195A4'))
-        else:
-            brush = QBrush(QColor('#13BE4B'))
-        painter.setBrush(brush)
-        painter.drawEllipse(circle_rect)
-
-        # 텍스트 그리기
-        font = QFont("Arial", 15)
-        painter.setFont(font)
-        painter.setPen(Qt.white)  # 텍스트 색상
-        painter.drawText(self.rect(), Qt.AlignCenter, self.text)
+from label_ui import CircleLabel
 
 
 class lotto_Result(QDialog):
