a4da0ab29182c52a2faa4342f9c5cc0b9525060d
zzzz955 <zzzzz955@gmail.com>
2023-09-04 14:38:48 +0900

    mainwindow의 함수 별도의 모듈로 분리

--- mainwindow.py
+++ mainwindow.py
@@ -2,12 +2,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, \
     QHBoxLayout, QPushButton, QLineEdit, QGridLayout, QMessageBox
 from PyQt5.Qt import QIntValidator
 from qt_material import apply_stylesheet
-import webbrowser
-import os
 from dialog import *
-import crawling
-import to_json
-import to_excel_csv
 
 
 class MainWindow(QMainWindow):
@@ -61,40 +56,12 @@ class MainWindow(QMainWindow):
             result_dialog = lotto_Result(self, int(num))
             result_dialog.exec()
 
+    def con_prize_page(self, index):
+        mainfunc.web_view(index)
+
     def show_dialog(self):
         dialog = download_Lotto_Prize_Value(self)
         dialog.show()
-
-    def con_prize_page(self, index):
-        try:
-            if index:
-                url = ('https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query='
-                       + index +
-                       '%ED%9A%8C%20%EB%A1%9C%EB%98%90%EB%8B%B9%EC%B2%A8%EB%B2%88%ED%98%B8')
-                webbrowser.open(url)
-        except Exception as e:
-            print(e)
-
-    def fending_json(self, file_path, first_num, last_num):
-        data = crawling.find_prize_number(first_num, last_num)
-        to_json.download_json(file_path, data)
-        self.open_file(file_path)
-
-    def fending_excel(self, file_path, first_num, last_num):
-        data = crawling.find_prize_number(first_num, last_num)
-        to_excel_csv.download_excel(file_path, data)
-        self.open_file(file_path)
-
-    def fending_csv(self, file_path, first_num, last_num):
-        data = crawling.find_prize_number(first_num, last_num)
-        to_excel_csv.download_csv(file_path, data)
-        self.open_file(file_path)
-
-    def open_file(self, file_path):
-        result = QMessageBox.information(self, '파일 저장 완료', '성공 적으로 파일을 저장 하였습니다. 저장된 파일을 확인해 보시겠습니까?',
-                                         QMessageBox.Ok | QMessageBox.No, QMessageBox.Ok)
-        if result == QMessageBox.Ok:
-            os.startfile(file_path)
 
     def close_app(self):
         # 앱 종료
