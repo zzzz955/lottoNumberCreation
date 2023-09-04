c09c9fa8ad4dd0897fb5a736e1eed9c1b784d0bf
zzzz955 <zzzzz955@gmail.com>
2023-09-04 14:08:08 +0900

    csv 파일 저장 시 한글 깨짐 현상 수정

--- to_excel_csv.py
+++ to_excel_csv.py
@@ -3,15 +3,11 @@ import openpyxl
 
 
 def download_excel(file_path, data):
-    result = []
-    result.append(data)
-    df = pd.DataFrame(result, index=None)
+    df = pd.DataFrame(data, index=None)
     df.to_excel(file_path, sheet_name='result', index=False, engine='openpyxl')
 
 
 def download_csv(file_path, data):
-    result = []
-    result.append(data)
-    df = pd.DataFrame(result, index=None)
-    df.to_csv(file_path, index=False)
+    df = pd.DataFrame(data, index=None)
+    df.to_csv(file_path, encoding='euc-kr', index=False)
 
