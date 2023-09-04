872a923a69ff4c53cb47f5e39750d6bcefda62b5
zzzz955 <zzzzz955@gmail.com>
2023-09-04 14:08:20 +0900

    json 파일 저장 시 한글 깨짐 현상 수정

--- to_json.py
+++ to_json.py
@@ -2,7 +2,7 @@ import json
 
 
 def download_json(filepath, data):
-    json_data = json.dumps(data)
-    with open(filepath, 'w') as json_file:
-        json.dump(json_data, json_file)
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    with open(filepath, 'w', encoding='euc-kr') as json_file:
+        json_file.write(json_data)
 
