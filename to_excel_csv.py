d21df87b89e3fcc75b91ed0418c5da79d09d50d1
zzzz955 <zzzzz955@gmail.com>
2023-09-06 18:43:04 +0900

    추첨 결과 엑셀 파일 다운로드 기능 추가

--- to_excel_csv.py
+++ to_excel_csv.py
@@ -11,3 +11,14 @@ def download_csv(file_path, data):
     df = pd.DataFrame(data, index=None)
     df.to_csv(file_path, encoding='euc-kr', index=False)
 
+
+def download_result(file_path, data):
+    data_lists = []
+    col = []
+    for i in range(6):
+        col.append(f'추첨 번호{i+1}')
+    for i in range(0, len(data), 6):
+        data_lists.append(data[i:i + 6])
+    df = pd.DataFrame(data_lists, columns=col)
+    df.to_excel(file_path, sheet_name='result', index=False, engine='openpyxl')
+
